b92713ac7d23d82646b01d2234aff45228906ffe
SergioT15 <sergiotitov15@gmail.com>
2022-12-27 15:10:42 +0300

    Add first and third tasks

--- main.js
+++ main.js
@@ -1,6 +1,121 @@
-alert("Hello");
-alert("How are you?");
-alert("How are you?");
-alert("How are you?");
-alert("How are you?");
-alert("How are you?");
+//1
+
+// 1. Функция. Принимает массив строк.
+//  Должна вернуть массив результатов проверки двух строк.
+//  Если у одной строки с последующей первый и последний символы, то true.
+//  Например ["asd", "afffd", "cc", "kk"].
+//  Для такого массива функция должна вернуть [true, false, false]
+
+// let array = ["asd", "afffd", "cc", "kk"];
+
+// function characterComparison(array) {
+//   return array
+//     .map(
+//       (item, i, arr) =>
+//         arr[i + 1] &&
+//         item[0] === arr[i + 1][0] &&
+//         item[item.length - 1] === arr[i + 1][arr[i + 1].length - 1]
+//     )
+//     .slice(0, -1);
+// }
+
+// console.log(characterComparison(array));
+
+
+//3
+
+// 3. На вход приниаем массив целых чисел, который должен быть преобразован по следующим правилам:
+// - если последвательность составляет диапазон из последовательных целых чисел (как минимум 3 числа): '1, 2, 3, 4' = '1 - 4',
+// - в остальных случая числа разделены запятой
+// Example:
+// solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// returns "-6,-3-1,3-5,7-11,14,15,17-20"
+
+
+
+// let arr3 = [
+//   -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
+// ];
+
+// function makeRage(arr) {
+//   const finalArr = [];
+//   let intermediateArr = [];
+
+//   function mergeArr() {
+//     let arrToMerge = intermediateArr;
+    
+//     console.log(arrToMerge);
+//     if (intermediateArr.length >= 3) {
+//       const firstValue = intermediateArr[0];
+//       const lastValue = intermediateArr[intermediateArr.length - 1];
+//       arrToMerge = [`${firstValue} - ${lastValue}`];
+//     }
+//     finalArr.push(...arrToMerge);
+//     intermediateArr = [];
+//   }
+
+//   for (const [, value] of arr.entries()) {
+//     const lastValue = intermediateArr[intermediateArr.length - 1];
+//     if (lastValue === undefined || value === lastValue + 1) {
+//       intermediateArr.push(value);
+//     } else {
+//       mergeArr();
+//       intermediateArr = [value];
+//     }
+//   }
+
+//   return finalArr.join(",");
+// }
+// console.log(makeRage(arr3));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Написать приложение, получающее массив с вложенными массивами и возвращающее его “плоскую” версию.
+// Встроенный метод массивов flat использовать нельзя.
+// Например: [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]] => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let array2 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+
+// let  =[]
+// const unpack = (item)
+
+
+
+
+
+
+
+
+
+
+
+
+// for (item of array3){
+
+//   let same = []
+//   if (array3[i + 1] - item === 1) {
+//     same.push;
+//     console.log(same);
+//   }
+// }
+
+// function characterComparison(array) {
+//   return array
+//     .map(
+//       (item, i, arr) =>
+//         arr[i + 1] &&
+//         item[0] === arr[i + 1][0] &&
+//         item[item.length - 1] === arr[i + 1][arr[i + 1].length - 1]
+//     )
+//     .slice(0, -1);
+// }
+
+//  array3.reduce((item, i, arr) => {
+//   let same = [];
+//   if (arr[i + 1] - item === 1) {
+//     same.push;
+//   }
+//   return same;
+// });
+
+// console.log();
